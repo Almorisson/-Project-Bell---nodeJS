24b479fa283ebbe315477eff9a23d3981def63ad
Mor NDIAYE <mo.ndiaye@ecole-ipssi.net>
2020-03-24 00:21:55 +0100

    [hot-fix] : Fix a crash discover after last commit deadline on Master Branch
     Extras infos you want to add

--- Config/dev.example.js
+++ Config/dev.example.js
@@ -4,5 +4,6 @@ module.exports = {
 	MONGO_PORT: 27017,
 	MONGO_URI_LOCAL: 'mongodb://127.0.0.1:27017/db', // To customize on your need
 	MONGO_URI_ATLAS: '', // To customize on your need,
-    // And all others config vars
+    JWT_SECRET_KEY: "94567898673tyrzgrtyupgjjf12",
+    PRIVATE_KEY: "random_JkLY9qfOJ8viaL1Ij1c8Qkcy00lbB8ZvFw"
 };
